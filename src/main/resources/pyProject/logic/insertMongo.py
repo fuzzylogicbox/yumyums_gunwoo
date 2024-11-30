005e7a5bf04e0d0ce94debe8ed8ddab7b2689026
fuzzylogicbox <158565734+fuzzylogicbox@users.noreply.github.com>
2024-11-30 23:50:48 +0900

    Update insertMongo.py

--- src/main/resources/pyProject/logic/insertMongo.py
+++ src/main/resources/pyProject/logic/insertMongo.py
@@ -23,7 +23,6 @@ cursor = mysql_conn.cursor()
 cursor.execute("SELECT id, title,category,content FROM faq ")
 rows = cursor.fetchall()
 
-api_key = "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"
 #
 url = "https://api.openai.com/v1/embeddings"
 headers = {
