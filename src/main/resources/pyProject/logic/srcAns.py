c4f5a1b8f2fe34e6ae8cb477122c6f2daf30af5a
fuzzylogicbox <158565734+fuzzylogicbox@users.noreply.github.com>
2024-11-30 23:42:42 +0900

    Update srcAns.py

--- src/main/resources/pyProject/logic/srcAns.py
+++ src/main/resources/pyProject/logic/srcAns.py
@@ -10,6 +10,7 @@ import nltk
 # user_input = "정기구매 방식"ㄴㄴ
 # OpenAI API 설정
 #
+api_key = "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"
 url = "https://api.openai.com/v1/embeddings"
 headers = {
     "Content-Type": "application/json",
